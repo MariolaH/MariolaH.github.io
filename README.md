fa68e709135eabad55959323bb99d9db459e42b9
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-04-10 19:00:32 +0000

    Revert "add"
    This reverts commit d9bbf55cd65558bab15f6ca35e7ee742b8a8f0cd.

---
+++ README.md
@@ -0,0 +1,1 @@
+# MariolaH.github.io
---
+++ portfolio/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Css/Portfolio.css";
+import "bootstrap/dist/css/bootstrap.css";
+// import Navbar from './Component/Navbar';
+import HeaderMain from './Component/HeaderMain';
+import MainTeaser from './Component/MainTeaser';
+import NavNew from './Component/NavNew';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <>
+   {/* <Navbar /> */}
+   <NavNew />
+   <HeaderMain />
+   <MainTeaser />
+    </>
+  );
+}
+
+export default App;
