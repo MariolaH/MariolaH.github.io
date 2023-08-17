56cb61fa101423e2cac4d1ee5410c24cf5774f44
MariolaH <mariolahullings@gmail.com>
2023-08-17 01:47:31 +0000

    updates

--- portfolio/src/Component/Bio.js
+++ portfolio/src/Component/Bio.js
@@ -8,7 +8,7 @@ function Bio() {
       <div className="frame">
         <section>
           <div className="container">
-            <div className="row">
+            <div className="row d-flex justify-content-right py-5">
               <div className="col-6 d-flex py-5">
                 <img
                   src="./Image/Profile p.jpg"
@@ -16,17 +16,17 @@ function Bio() {
                   alt="Mariola Hullings"
                 />
               </div>
-              <div className="col-6 d-flex justify-content-right py-5">
+              <div className="col-6 d-flex justify-content-right py-5 wrap-under">
                 <p className="bioParagraph">
-                  Hello, my name is Mariola and reside in the Kentucky 
-                  area. I have a passion for keeping active, especially with
-                  activities like hitting the gym and hiking. A good friend
-                  introduced me to the Awesome Inc bootcamp. Following their
-                  lead, I took on the challenge, rounding it off with a 10-week
-                  iOS internship. That experience sealed my love for mobile
-                  development. Apart from coding, I have a deep appreciation for
-                  travel, I enjoy cooking up new dishes, and I never miss an
-                  opportunity for a good hike.
+                  Hello, my name is Mariola and reside in the Kentucky area. I
+                  have a passion for keeping active, especially with activities
+                  like hitting the gym and hiking. A good friend introduced me
+                  to the Awesome Inc bootcamp. Following their lead, I took on
+                  the challenge, rounding it off with a 10-week iOS internship.
+                  That experience sealed my love for mobile development. Apart
+                  from coding, I have a deep appreciation for travel, I enjoy
+                  cooking up new dishes, and I never miss an opportunity for a
+                  good hike.
                 </p>
               </div>
 
