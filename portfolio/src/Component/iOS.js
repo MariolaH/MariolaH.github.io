9b0943682639508011c6347a4b2572d63bc440da
MariolaH <mariolahullings@gmail.com>
2023-11-25 20:52:31 +0000

    added new project to iOS

--- portfolio/src/Component/iOS.js
+++ portfolio/src/Component/iOS.js
@@ -14,19 +14,19 @@ function Ios() {
           <b className="projectTitle">i O S &nbsp; P R O J E C T S</b>
         </h1>
         <section>
-          {/* <div className="containerBlog">
+          <div className="containerBlog">
             <div className="row d-flex justify-content-center align-items-center py-3">
               <div className="col-sm-6 d-flex justify-content-center align-items-center py-3">
                 <div className="card">
                   <video
                     class="card-img-top"
-                    src="./Videos/tip:splitCalc.mov"
+                    src="./Videos/WeatherApp.mov"
                     height={350}
                     alt="Weather app"
                     controls
                   ></video>
                   <div className="card-body">
-                    <h4 className="card-title"> WEATHER APP </h4>
+                    <h4 className="card-title"> THE WEATHER APP </h4>
                     <br />
                     <p className="card-text">Tech Stack: UIKit</p>
                     <a
@@ -44,7 +44,7 @@ function Ios() {
                 </div>
               </div>
             </div>
-          </div> */}
+          </div>
 
           <div className="containerBlog">
             <div className="row d-flex justify-content-center align-items-center py-3">
