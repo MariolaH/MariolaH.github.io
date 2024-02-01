6e5b82aa123dc0b5d07f8fce69cb3d84290d22e0
MariolaH <mariolahullings@gmail.com>
2024-02-01 01:27:45 +0000

    added new project to iOS section

--- portfolio/src/Component/iOS.js
+++ portfolio/src/Component/iOS.js
@@ -21,6 +21,38 @@ function Ios() {
                 <div className="card">
                   <video
                     class="card-img-top"
+                    src="./Videos/DiceApp.mp4"
+                    height={350}
+                    alt="Weather app"
+                    controls
+                  ></video>
+                  <div className="card-body">
+                    <h4 className="card-title"> LAS VEGAS DICE APP </h4>
+                    <br />
+                    <p className="card-text">Tech Stack: SwiftUI</p>
+                    <a
+                      rel="noreferrer"
+                      href="https://github.com/MariolaH/Dicee-SwiftUI"
+                      target="_blank"
+                    >
+                      <button className="btn btn-outline-secondary">
+                        View Repo
+                      </button>
+                    </a>
+                    <br />
+                    <br />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="containerBlog">
+            <div className="row d-flex justify-content-center align-items-center py-3">
+              <div className="col-sm-6 d-flex justify-content-center align-items-center py-3">
+                <div className="card">
+                  <video
+                    class="card-img-top"
                     src="./Videos/Chat.mp4"
                     height={350}
                     alt="Weather app"
